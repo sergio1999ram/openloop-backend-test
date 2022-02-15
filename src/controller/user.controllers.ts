9296367fe88b555c149c415c26b82c0068947d94
Sergio <sramirezpch@gmail.com>
2022-02-14 22:37:32 -0500

    Added functionality to controllers

--- src/controller/user.controllers.ts
+++ src/controller/user.controllers.ts
@@ -1,28 +1,55 @@
 import { User } from "../interface/user.interface";
 import { CreateUser, DeleteUser, ListUsers, UpdateUser } from "../types";
 
-export const listUsers: ListUsers = (req, res) => {
-    const user: User = {
-        id: '1',
-        firstName: 'Sergio',
-        lastName: 'Ramirez',
-        note: 'Test'
+import UserSchema from '../schemas/user.schema';
+import { Types } from "mongoose";
+
+export const listUsers: ListUsers = async (req, res) => {
+    try {
+        const users = await UserSchema.find();
+        res.status(200).json({ users })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: 'Couldn\'t get the users, something happened!' })
+    }
+};
+
+export const createUser: CreateUser = async (req, res) => {
+    const { firstName, lastName, note } = req.body;
+
+    try {
+        const user = await UserSchema.create({ firstName, lastName, note });
+        res.status(201).json({ ok: true, user });
+    } catch (error: any) {
+        console.log(error);
+        res.status(400).json({ errors: error?.errors })
+    }
+};
+
+export const updateUser: UpdateUser = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const updatedUser = await UserSchema.updateOne({ _id: new Types.ObjectId(id) }, req.body);
+        if (updatedUser.matchedCount === 0)
+            res.status(404).json({ message: 'No user found to update' });
+        res.status(200).json({ message: 'User updated successfully' })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error })
+    }
+};
+
+export const deleteUser: DeleteUser = async (req, res) => {
+    const { id } = req.params;
+    console.log(id);
+    try {
+        const deletedUser = await UserSchema.deleteOne({ _id: new Types.ObjectId(id) });
+        if (deletedUser.deletedCount === 0)
+            res.status(404).json({ message: 'No user found to delete' })
+        res.status(200).json({ message: 'User deleted successfully' })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error })
     };
-
-    res.status(200).json({ user: [user] })
-};
-
-export const createUser: CreateUser = (req, res) => {
-    const userId = req.params.id;
-    console.log(userId);
-};
-
-export const updateUser: UpdateUser = (req, res) => {
-    const userId = req.params.id;
-    console.log(userId);
-};
-
-export const deleteUser: DeleteUser = (req, res) => {
-    const userId = req.params.id;
-    console.log(userId);
 };
