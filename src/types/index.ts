a60a40bdb7d003d860634d9b91720757a1cb73fd
Sergio <sramirezpch@gmail.com>
2022-02-14 20:42:42 -0500

    Created types for the user routes controllers

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,6 @@
 import { Request, Response } from 'express';
 
 export type ListUsers = (req: Request, res: Response) => void;
+export type CreateUser = (req: Request, res: Response) => void;
+export type UpdateUser = (req: Request, res: Response) => void;
+export type DeleteUser = (req: Request, res: Response) => void;
